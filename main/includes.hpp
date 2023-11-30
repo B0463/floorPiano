2f34fd6ad4fde3eabe9206d5550a80062ea9665b
Henri Kim Pinheiro Rodrigues <hkimpr40@gmail.com>
2023-11-30 00:29:44 -0300

    adjustments, otimization and comments

--- main/includes.hpp
+++ main/includes.hpp
@@ -1,24 +1,56 @@
 #pragma once
 
-//consts
-#define BOARD 4
-//#define DEBUG
-#define TOUCHSENSE 900
-#define SAMPLINGINTERVAL 5
-#define WINDOWSIZE 15
+// Config
+#define BOARD 1            // Board to upload code (1-4)
+#define DEBUG              // If defined, tx the filtred values of inputs
+#define TOUCHSENSE 600     // (0-1023)
+#define SAMPLINGINTERVAL 3 // Time(ms) between samples (0-255)
+#define WINDOWSIZE 8       // Number of samplings (0-255)
 
-//defs
-void MaFilter(int si=10, int ws=20, int av[7]={0});
+// MaFilter define ( si=samplingInterval, ws=windowSize, av=analogValues )
+void MaFilter(uint8_t si=10, uint8_t ws=20, uint16_t av[7]={0});
 
-//board include
+// Board key ( F=fliped )
 #if defined(BOARD) && BOARD == 1
-    #include "board1.hpp"
+    uint16_t keyTones[7] = {
+        131, // 2C
+        234, // 2A# F1
+        147, // 2D
+        221, // 2A  F2
+        166, // 2E
+        175, // 2F
+        197, // 2G  F3
+    };
 #elif defined(BOARD) && BOARD == 2
-    #include "board2.hpp"
+    uint16_t keyTones[7] = {
+        186  // 2F# F3
+        209, // 2G#
+        157, // 2D# F2
+        139, // 2C# F1
+        249, // 2B
+        313, // 3D# F4
+        279  // 3C#
+    };
 #elif defined(BOARD) && BOARD == 3
-    #include "board3.hpp"
+    uint16_t keyTones[7] = {
+        295, // 3D
+        262, // 3C  F4
+        330, // 3E
+        351, // 3F
+        442, // 3A  F5
+        394, // 3G
+        468, // 3A# F6
+    };
 #elif defined(BOARD) && BOARD == 4
-    #include "board4.hpp"
+    uint16_t keyTones[7] = {
+        372, // 3F# F5
+        417  // 3G# F6
+        496, // 3B
+        525, // 4C
+        556, // 4C#
+        589, // 4D
+        0    // N/C
+    };
 #else
-    #error "Selecione uma placa válida"
+    #error "Select a valid board"
 #endif
--- main/main.ino
+++ main/main.ino
@@ -1,9 +1,9 @@
 #include "includes.hpp"
 
 void setup() {
-    pinMode(13, OUTPUT);
-    pinMode(3, OUTPUT);
-    Serial.begin(115200);
+    pinMode(13, OUTPUT);  // Built in
+    pinMode(3, OUTPUT);   // Sound output
+    Serial.begin(115200); // Serial rate 115200b/s
     pinMode(A1, INPUT);
     pinMode(A2, INPUT);
     pinMode(A3, INPUT);
@@ -12,55 +12,52 @@ void setup() {
     pinMode(A6, INPUT);
     pinMode(A7, INPUT);
 }
-
+bool biState=0; // Blink state
 void loop() {
-    int touchSense = TOUCHSENSE;
-    int samplingInterval = SAMPLINGINTERVAL;
-    int windowSize = WINDOWSIZE;
-    int analogValues[7]={0};
-    bool keyValues[7];
-    MaFilter(samplingInterval, windowSize, analogValues);
-    for(int i=0;i<7;i++) {
-        keyValues[i] = (analogValues[i] < touchSense) ? 1 : 0;
-    }
+    uint16_t analogValues[7]={0};
+    MaFilter(SAMPLINGINTERVAL, WINDOWSIZE, analogValues); // Put filtred values in analogValues
     #ifdef DEBUG
-    Serial.write(27);
-    Serial.print("[2J");
-    Serial.write(27);
-    Serial.print("[H");
-    Serial.println("Port|Value |Press");
-    for(int i=0;i<7;i++) {
-        int value = analogValues[i];
-        Serial.print("A" + String(i+1) + "  |");
-        if(value < 10) Serial.print("    ");
-        else if(value < 100) Serial.print("   ");
-        else if(value < 1000) Serial.print("  ");
-        else Serial.print(" ");
-        Serial.print(String(value) + " | " + String(keyValues[i]));
-        Serial.println();
-    }
+        Serial.write(27);    // Serial clear
+        Serial.print("[2J"); //
+        Serial.write(27);    //
+        Serial.print("[H");  //
+        Serial.println("Port|Value |Press"); // Header
+        for(uint8_t i=0;i<7;i++) {
+            uint16_t value = analogValues[i];
+            Serial.print("A" + String(i+1) + "  |");
+            if(value < 10) Serial.print("    ");      // Number spacing
+            else if(value < 100) Serial.print("   "); //
+            else if(value < 1000) Serial.print("  "); //
+            else Serial.print(" ");                   //
+            Serial.print(String(value) + " | " + String((analogValues[i] < TOUCHSENSE) ? 1 : 0)); // Print data
+            Serial.println();
+        }
+    #else
+        // Flip Built in led state
+        digitalWrite(13, !biState);
+        biState=!biState;
     #endif
-    bool uso=0;
-    for(int i=0;i<7;i++) {
-        if(keyValues[i]) {tone(3, keyTones[i]); uso=1;}
+    bool use=0;
+    for(uint16_t i=0;i<7;i++) {
+        if(analogValues[i] < TOUCHSENSE) {tone(3, keyTones[i]); use=1; break;} // Play key
     }
-    if(!uso) noTone(3);
+    if(!use) noTone(3); // Stop key
 }
 
-void MaFilter(int si=10, int ws=20, int av[7]={0}) {
-    int avBuffer[ws][7];
-    for(int i=0;i<ws;i++) {
-        for(int j=0;j<7;j++) {
-            avBuffer[i][j] = analogRead(j+1);
+void MaFilter(uint8_t si=10, uint8_t ws=20, uint16_t av[7]={0}) {
+    uint16_t avBuffer[ws][7]; // Storage all raw samples
+    for(uint8_t i=0;i<ws;i++) {
+        for(uint8_t j=0;j<7;j++) {
+            avBuffer[i][j] = analogRead(j+1); // Samplings
         }
         delay(si);
     }
-    for(int i=0;i<ws;i++) {
-        for(int j=0;j<7;j++) {
-            av[j] = av[j] + avBuffer[i][j];
+    for(uint8_t i=0;i<ws;i++) {
+        for(uint8_t j=0;j<7;j++) {
+            av[j] = av[j] + avBuffer[i][j]; // Add all raw samples
         }
     }
-    for(int i=0;i<7;i++) {
-        av[i] = av[i]/ws;
+    for(uint8_t i=0;i<7;i++) {
+        av[i] = av[i]/ws; // Div all added samples
     }
 }
